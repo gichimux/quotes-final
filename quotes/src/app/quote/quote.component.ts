53df6d66f08ed5cc1344f3bfb287af4299cb71f6
gichimu mwai <gichimumwai1@mail.com>
2019-03-25 16:11:44 +0300

    added buttons and event listeners

--- quotes/src/app/quote/quote.component.ts
+++ quotes/src/app/quote/quote.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quotes'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote(0, 'lorem ipsum lorem', 'lorem ipsum', 'john doe',new Date(2019, 3, 4)),
+    new Quote(0,'lorem ipsum lorem', 'lorem ipsum', 'jane doe',new Date(2019, 3, 4)),
+    new Quote(0,'lorem ipsum lorem', 'lorem ipsum', 'june doe',new Date(2019, 3, 4)),
+    new Quote(0,'lorem ipsum lorem', 'lorem ipsum', 'july doe',new Date(2019, 3, 4)),
+    new Quote(0,'lorem ipsum lorem', 'lorem ipsum', 'jon doe',new Date(2019, 3, 4))
+
+  ]
+
+  quoteDelete(isComplete, Index){
+    if(isComplete){
+      let toDelete = confirm (`Are you sure you want to DELETE ${this.quotes[Index].postedQuote}?`)
+      if (toDelete){
+        this.quotes.splice(Index,1)
+      }
+    }
+  }
+
+  toggleDetails(Index){
+    this.quotes[Index].showDetails = !this.quotes[Index].showDetails;
+  }
+
+   addNewQuote(quote){
+      let quoteLength= this.quotes.length;
+      quote.upVote = 0;
+      quote.timePosted = new Date(quote.timePosted);
+      this.quotes.push(quote)
+    }
 
   constructor() { }
 
