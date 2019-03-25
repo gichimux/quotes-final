e081ad8227227fceaf0a8cbdcbf144549911765f
gichimu mwai <gichimumwai1@mail.com>
2019-03-25 16:13:24 +0300

    added source folder

---
+++ quotes/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UpvoteHighlightDirective } from './upvote-highlight.directive';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteComponent } from './quote/quote.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UpvoteHighlightDirective,
+    QuotesFormComponent,
+    QuoteDetailsComponent,
+    QuoteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- quotes/src/app/quotes.ts
+++ quotes/src/app/quotes.ts
@@ -1,6 +1,6 @@
-export class Quotes {
+export class Quote {
   public showDetails: boolean;
-  constructor(public upVote: number, public downVote: number, public name: string, public details: string, public timePosted: Date){
+  constructor(public upVote: number, public postedQuote: string, public authorName: string, public userName: string, public timePosted: Date){
       this.showDetails = false
   }
 }
